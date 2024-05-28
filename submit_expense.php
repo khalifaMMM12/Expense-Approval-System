62c3639a159b8a0aab1cde26f85e5288dfddc781
Mudathir M Muhammad <khalifasitemsn@gmail.com>
2024-05-28 01:06:50 +0100

    Update submit_expense.php

--- submit_expense.php
+++ submit_expense.php
@@ -73,6 +73,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit_expense"])) {
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Expense Approval System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="submit_expense.php">Submit Expense</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="container mt-4">
         <h2>Submit Expense</h2>
         <?php 
